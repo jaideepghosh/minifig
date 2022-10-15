817370c4e0933a120a48af49f729ff3e4f613adc
jaideepghosh <er.jaideepghosh@gmail.com>
2022-10-16 01:34:44 +0530

    feat:Added common context and updated the app

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import Head from "next/head";
+import CommonContext from "../contexts/common.context";
+
 export default function Home() {
+  const [common, setCommonContext] = useState({
+    showTray: false,
+    selectedNavItem: null,
+  });
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <CommonContext.Provider value={{ common, setCommonContext }}>
+      <Head>
+        <title>minifig - Create the coolest avatar.</title>
+        <meta
+          name="description"
+          content="minifig - Create the coolest avatar."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen sticky top-0"></div>
+    </CommonContext.Provider>
   );
 }
